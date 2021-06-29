3983306f06f54f4a76fafee1efa61f6b064483e0
291564.umk@gmail.com <291564.umk@gmail.com>
2021-06-30 00:20:20 +0200

    Adding side components: structure and properties

--- tinymce-editor/src/app/app.module.ts
+++ tinymce-editor/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EditorModule } from '@tinymce/tinymce-angular';
 import { AppComponent } from './app.component';
 import { EditorComponent } from './components/editor/editor.component';
+import { TreeStructureComponent } from './components/tree-structure/tree-structure.component';
+import { PropertiesComponent } from './components/properties/properties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorComponent
+    EditorComponent,
+    TreeStructureComponent,
+    PropertiesComponent
   ],
   imports: [
     BrowserModule,
