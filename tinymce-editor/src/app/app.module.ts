f8d85dd3df6e8e560a0d93b7620070d83fefab3f
291564.umk@gmail.com <291564.umk@gmail.com>
2021-06-30 00:40:12 +0200

    Adding essential modules for flat cdk tree

--- tinymce-editor/src/app/app.module.ts
+++ tinymce-editor/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { EditorComponent } from './components/editor/editor.component';
 import { TreeStructureComponent } from './components/tree-structure/tree-structure.component';
 import { PropertiesComponent } from './components/properties/properties.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { PropertiesComponent } from './components/properties/properties.componen
   ],
   imports: [
     BrowserModule,
-    EditorModule
+    EditorModule,
+    BrowserAnimationsModule,
+    CdkTreeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
