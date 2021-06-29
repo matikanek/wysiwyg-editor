0b68a256692a9dd5331139ea6bc1e31105c12da0
291564.umk@gmail.com <291564.umk@gmail.com>
2021-06-29 12:21:08 +0200

    Adding table and picture tool in toolbar and mechanizm selecting active cell from table

--- tinymce-editor/src/app/components/editor/editor.component.ts
+++ tinymce-editor/src/app/components/editor/editor.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
-
+export class EditorComponent {
+  activeField = null;
   constructor() { }
 
-  ngOnInit(): void {
+  clickOnEditor(event: any){
+    this.resetActive(event);
+    this.setActive(event);
+    if(this.activeField == event.editor.getBody()){
+      this.resetActive(event);
+    }
   }
 
+  resetActive(event: any): void {
+    if(this.activeField != null){
+      event.editor.dom.setStyle(this.activeField, 'background-color', 'transparent');
+      event.editor.dom.setStyle(this.activeField, 'border', '1px solid #ccc');
+    }
+  }
+
+  setActive(event: any): void {
+    this.activeField = event.editor.selection.getNode();
+    if(this.activeField != event.editor.getBody()){
+      let element = this.activeField as any;
+      while(element.nodeName != 'TD') {
+        this.activeField = element.parentNode;
+        element = element.parentNode;
+      }
+      event.editor.dom.setStyle(this.activeField, 'background-color', '#cce9ff');
+      event.editor.dom.setStyle(this.activeField, 'border', 'solid #5591cf');
+    }
+  }
 }
