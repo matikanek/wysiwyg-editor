7060a35a6a70c4f559d6ac56a1f13437feaef079
291564.umk@gmail.com <291564.umk@gmail.com>
2021-06-29 12:10:02 +0200

    TinyMCE - get started by Angular framework

---
+++ tinymce-editor/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { AppComponent } from './app.component';
+import { EditorComponent } from './components/editor/editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EditorComponent
+  ],
+  imports: [
+    BrowserModule,
+    EditorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
