5709c28d86b75debb2074daa784baa4e77c568d4
291564.umk@gmail.com <291564.umk@gmail.com>
2021-07-01 23:37:20 +0200

    Adding more features

--- tinymce-editor/src/app/components/editor/editor.component.ts
+++ tinymce-editor/src/app/components/editor/editor.component.ts
@@ -42,9 +42,70 @@ export class EditorComponent {
     return true;
   }
 
+  clickOnTreeItem(event: any): void {
+    this.removeActiveFromTree();
+    let nodeName = event.toElement.innerText;
+    let selectedElementIndex = this.getSelectecElementIndex(event, nodeName);
+    let selectedElementHandle = document.getElementsByTagName('iframe')[0]
+      .contentWindow?.document.getElementsByTagName(nodeName.trim())[selectedElementIndex];
+    this.disableCurrentActiveElement()
+    this.setActiveOnEditor(selectedElementHandle);
+  }
+
+  setActiveOnEditor(node: any): void {
+    node.style.backgroundColor = '#cce9ff';
+    node.style.border = 'solid #5591cf';
+  }
+
+  removeActiveFromTree(): void {
+    let active = document.getElementsByClassName('editor__container-structure-active-item')[0];
+    //console.log(active);
+    if(active != null) {
+      //for(let i=0; i<active.length; i++) {
+        active.classList.remove('editor__container-structure-active-item');
+      //}
+    }
+  }
+
+  disableCurrentActiveElement(): void {
+    let editorTags = 
+      Array.prototype.slice.call(
+        document.getElementsByTagName('iframe')[0]
+        .contentWindow?.document.getElementsByTagName('*')
+      )
+      .filter((item)=>{
+        return this.listOfTags.includes(item.nodeName.toLocaleLowerCase());
+      });
+    for(let i=0; i<editorTags.length; i++) {
+      if(editorTags[i].style.backgroundColor == 'rgb(204, 233, 255)') {
+        editorTags[i].style.backgroundColor = 'transparent';
+        editorTags[i].style.border = '1px solid #ccc';
+        break;
+      }
+    }
+  }
+
+  getSelectecElementIndex(event: any, node: string): number {
+    let selectedElement = event.path[1];
+    selectedElement.setAttribute('flex-direction', 'row');
+    selectedElement.children[0].classList.add('editor__container-structure-active-item');
+    let collection = Array.prototype.slice.call(event.path[2].children).filter((item: any)=> {
+      return item.innerText.trim() == node.trim();
+    });
+    let selectedElementIndex = 0;
+    for(let i=0; i<collection.length; i++) {
+      if(collection[i].getAttribute('flex-direction') == 'row'){
+        selectedElementIndex = i;
+      }
+    }
+    selectedElement.removeAttribute('flex-direction');
+    return selectedElementIndex;
+  }
+
   clickOnEditor(event: any){
+    this.removeActiveFromTree()
     this.resetTree();
-    this.resetActive(event);
+    this.disableCurrentActiveElement();
     this.setActive(event);
     this.clearActive(event);
     this.initTree(event);
@@ -53,6 +114,8 @@ export class EditorComponent {
     node => node.level, node => node.expandable);
     this.dataSource = new ArrayDataSource(this.dataTree);
     this.hasChild = (_: number, node: FlatNode) => node.expandable;
+
+    console.log(this.dataTree);
   }
 
   clearActive(event: any): void {
@@ -149,28 +212,24 @@ export class EditorComponent {
         event.editor.dom.setStyle(this.activeField, 'border', 'solid #5591cf');
       }
     }
+    this.resetTree();
   }
 
   funkcja(event: any): void {
     let activeElement = this.activeField as any;
-    let activeItem: ActiveElement = {
-      isImage: false, font: '', size: '', weight: '', color: '', coords: {x: 0, y: 0}, width: 0, height: 0
-    };
     if(activeElement.nodeName != 'IMG') {
-      activeItem.isImage = false;
-      activeItem.font = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('font-family');
-      activeItem.size = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('font-size');
-      activeItem.weight = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('font-weight');
-      activeItem.color = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('color');
-      console.log(activeItem);
+      this.activeElement.isImage = false;
+      this.activeElement.font = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('font-family');
+      this.activeElement.size = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('font-size');
+      this.activeElement.weight = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('font-weight');
+      this.activeElement.color = window.getComputedStyle(event.editor.selection.getNode(), null).getPropertyValue('color');
     }
     else {
-      activeItem.isImage = true;
-      activeItem.coords.x = Math.round(event.editor.selection.getBoundingClientRect().x);
-      activeItem.coords.y = Math.round(event.editor.selection.getBoundingClientRect().y);
-      activeItem.width = event.editor.selection.getBoundingClientRect().width;
-      activeItem.height = event.editor.selection.getBoundingClientRect().height;
-      console.log(activeItem);
+      this.activeElement.isImage = true;
+      this.activeElement.coords.x = Math.round(event.editor.selection.getBoundingClientRect().x);
+      this.activeElement.coords.y = Math.round(event.editor.selection.getBoundingClientRect().y);
+      this.activeElement.width = Math.round(event.editor.selection.getBoundingClientRect().width);
+      this.activeElement.height = Math.round(event.editor.selection.getBoundingClientRect().height);
     }
   }
 }
